82814b27f93a83e83394c35c67aa3fa9e2a1187b
Lynxist <jimbobish@gmail.com>
2024-06-06 17:47:15 +0100

    [Lynx Crafts] Added Comments to config

--- config.lua
+++ config.lua
@@ -1,19 +1,19 @@
 Config = Config or {}
 
-Config.debug = false
-Config.boxZone = false
+Config.debug = false -- Set to true to enable target zone debug
+Config.boxZone = false -- Set to true to use box zone instead of target entity, might set this to each prop in the future
 
 Config.craftObjects = {
     {   
-        name = "Furnace",
-        model = "prop_wooden_barrel",
-        coords = vector4(-110.25274658203, -13.871084213257, 69.519638061523, 161.33299255371),
-        targetCoords = vector4(-110.25274658203, -13.871084213257, 69.519638061523, 161.33299255371),
-        targetDist = 30.0,
-        craftType = "food",
+        name = "Furnace", -- Name will be used in the name of the target zone, example: Craft Furnace
+        model = "prop_wooden_barrel", -- Model of the prop
+        coords = vector4(-110.25274658203, -13.871084213257, 69.519638061523, 161.33299255371), -- Coords of the prop
+        targetCoords = vector4(-110.25274658203, -13.871084213257, 69.519638061523, 161.33299255371), -- Coords of the target zone (if boxZone true, this will be the center of the box)
+        targetDist = 30.0, -- Distance of the target zone
+        craftType = "food", -- Type of craft, will be used to get the items from Config.craftMenu
     },
     { 
-        name = "Workbench", 
+        name = "Workbench", -- Name will be used in the name of the target zone, example: Craft Workbench
         model = "gr_prop_gr_bench_02a", 
         coords = vector4(-506.14105224609, 177.62062072754, 82.157676696777, 89.190635681152),
         targetCoords = vector4(-506.14105224609, 177.62062072754, 83.157676696777, 89.190635681152),
@@ -23,18 +23,18 @@ Config.craftObjects = {
 }
 
 Config.craftMenu = {
-    ["food"] = {
-        label = "Food",
-        items = {
-            ["dried_food"] = {
-                name = "Dried Food",
-                description = "A dried food item.",
-                ingredients = {
-                    { name = "apple", label = "Apple", amount = 1 },
+    ["food"] = { -- This key should match the craftType in Config.craftObjects
+        label = "Food", -- Label of the category (displayed as a header in the menu)
+        items = { -- Items in the category
+            ["dried_food"] = { -- This key should match the item name in the items table
+                name = "Dried Food", -- Name of the item
+                description = "A dried food item.", -- Description of the item
+                ingredients = { -- Ingredients required to craft the item
+                    { name = "apple", label = "Apple", amount = 1 }, -- name = item name, label = item label, amount = amount required to craft
                     { name = "banana", label = "Banana", amount = 1 },
                     { name = "strawberry", label = "Strawberry", amount = 1 },
                 },
-                craftTime = 5000,
+                craftTime = 5000, -- Time in milliseconds to craft the item
             },
             ["cooked_meat"] = {
                 name = "Cooked Meat",
@@ -46,8 +46,8 @@ Config.craftMenu = {
             },
         }
     },
-    ["weapons"] = {
-        label = "Weapons",
+    ["misc"] = {
+        label = "Misc",
         items = {
             ["racing_gps"] = {
                 name = "Racing GPS",
